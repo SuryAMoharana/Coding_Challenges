f6c2a89b7274801dfddfef92c2c9873760819db6
Surya Moharana <suryachintu1994@gmail.com>
2023-03-22 21:55:00 +0530

    Initialization of project

---
+++ PokeworldApplication/src/main/java/com/pokeworld/PokeworldApplication.java
@@ -0,0 +1,13 @@
+package com.pokeworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeworldApplication.class, args);
+	}
+
+}
